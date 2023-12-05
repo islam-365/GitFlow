900ad54a6e532e6f592d148ce5355dd00ee3f86a
islam-365 <eslamx13@gmail.com>
2023-12-05 22:12:13 +0300

    feature 1 finished

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
         System.out.println("Hello and welcome!");
         System.out.println("Hello to development");
+        System.out.println("Hello from feature 1");
 
         }
     }
